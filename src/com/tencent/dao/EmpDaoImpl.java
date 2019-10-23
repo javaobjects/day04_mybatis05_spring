ae6d2aa3316b8b03840a4a368e4d303de97c7153
yanbo0039 <yanbo0039@yeah.net>
2019-10-23 18:51:52 +0800

    day04_mybatis05_spring

--- src/com/tencent/dao/EmpDaoImpl.java
+++ src/com/tencent/dao/EmpDaoImpl.java
@@ -1,30 +1,31 @@
 package com.tencent.dao;
 
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 
 import com.tencent.pojo.Emp;
 
-public class EmpDaoImpl implements IEmpDao {
+public class EmpDaoImpl extends SqlSessionDaoSupport implements IEmpDao {
 
-	private SqlSessionFactory factory;
-	
-	public EmpDaoImpl(SqlSessionFactory factory)
-	{
-		this.factory = factory;
-	}
+//	private SqlSessionFactory factory;
+//	
+//	public EmpDaoImpl(SqlSessionFactory factory)
+//	{
+//		this.factory = factory;
+//	}
 	
 	@Override
 	public Emp selectEmpByEmpno(int empno) {
 		
 		//1.获取SqlSession接口
-		SqlSession session = factory.openSession();
+//		SqlSession session = factory.openSession();
 		
 		//2.执行查询
-		Emp emp = (Emp)session.selectOne("selectEmpByEmpno", empno);
+		Emp emp = (Emp)this.getSqlSession().selectOne("selectEmpByEmpno", empno);
+		
+		//session.commit();
 		
 		//3.归还连接
-		session.close();
+//		session.close();
 		
 		return emp;
 	}
--- src/com/tencent/test/TestMybatis.java
+++ src/com/tencent/test/TestMybatis.java
@@ -14,14 +14,14 @@ public class TestMybatis {
 
 	public static void main(String[] args) throws IOException {
 		//1.获取SqlSessionFactory接口
-		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("sqlMapConfig.xml"));
-		
-		//2.获取EmpDao
-		IEmpDao empDao = new EmpDaoImpl(factory);
-		
-		//3.调用查询
-		Emp emp = empDao.selectEmpByEmpno(8888);
-		System.out.println(emp);
+//		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("sqlMapConfig.xml"));
+//		
+//		//2.获取EmpDao
+//		IEmpDao empDao = new EmpDaoImpl(factory);
+//		
+//		//3.调用查询
+//		Emp emp = empDao.selectEmpByEmpno(8888);
+//		System.out.println(emp);
 	}
 
 }
